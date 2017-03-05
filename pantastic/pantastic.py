8d803f39944e48e4818f929d46b3b72a7d2bdb43
Chris Read <centurix@gmail.com>
2017-03-06 00:15:28 +1000

    Stuff

--- pantastic/pantastic.py
+++ pantastic/pantastic.py
@@ -29,14 +29,122 @@ class Pantastic:
                 if not file_buffer:
                     break
 
-                numbers = re.findall('\d', file_buffer, re.MULTILINE | re.DOTALL)
-                if len(numbers) > 0:
-                    number_buffer = ''.join(numbers)
+                # Find groups of numbers between 4 and 19 characters in length
+                number_groups = re.finditer('\d{4,19}', file_buffer, re.MULTILINE | re.DOTALL)
 
-                logging.info('Here are all the numbers in the file %s' % number_buffer)
+                for number_group in number_groups:
+                    logging.info('Number group at %d: %s' % (number_group.start(), number_group.group(0)))
 
-                for index in range(0, len(number_buffer)):
-                    for card_length in range(12, 19):
-                        card = Card(number_buffer[index:index + card_length])
-                        if card.valid_luhn and card.issuer != 'Unknown':
-                            logging.info('Found card (%s): %s' % (card.issuer, number_buffer[index:index + card_length]))
+                # Are these groups parts of a credit card number?
+
+                # Different methods to find card numbers here. Numbers between 12 and 19 digits long
+                # 999999999999
+                # 9999999999999
+                # 99999999999999
+                # 999999999999999
+                # 9999999999999999
+                # 99999999999999999
+                # 999999999999999999
+                # 9999999999999999999
+
+                # 9999-9999-9999
+                # 9999-9999-9999-9
+                # 9999-9999-9999-99
+                # 9999-9999-9999-999
+                # 9999-9999-9999-9999
+                # 9999-9999-9999-9999-9
+                # 9999-9999-9999-9999-99
+                # 9999-9999-9999-9999-999
+
+                # 9999 9999 9999
+                # 9999 9999 9999 9
+                # 9999 9999 9999 99
+                # 9999 9999 9999 999
+                # 9999 9999 9999 9999
+                # 9999 9999 9999 9999 9
+                # 9999 9999 9999 9999 99
+                # 9999 9999 9999 9999 999
+
+                # Then look for quads in separate fields in a database
+
+                # spaced_numbers_19 = re.finditer('[^\d\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{3}[^\d\-\s]', file_buffer, re.MULTILINE | re.DOTALL)
+                # spaced_numbers_18 = re.finditer('[^\d\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{2}[^\d\-\s]', file_buffer, re.MULTILINE | re.DOTALL)
+                # spaced_numbers_17 = re.finditer('[^\d\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d[^\d\-\s]', file_buffer, re.MULTILINE | re.DOTALL)
+
+                # spaced_numbers_16 = re.finditer(
+                #     '[^0-9\- ]\d{4}[ ]\d{4}[ ]\d{4}[ ]\d{4}[^0-9\- ]',
+                #     file_buffer,
+                #     re.MULTILINE | re.DOTALL
+                # )
+
+                # spaced_numbers_15 = re.finditer('[^\d\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{3}[^\d\-\s]', file_buffer, re.MULTILINE | re.DOTALL)
+                # spaced_numbers_14 = re.finditer('[^\d\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{2}[^\d\-\s]', file_buffer, re.MULTILINE | re.DOTALL)
+                # spaced_numbers_13 = re.finditer('[^\d\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d[^\d\-\s]', file_buffer, re.MULTILINE | re.DOTALL)
+                #
+                # spaced_numbers_12 = re.finditer('[^\d\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}[^\d\-\s]', file_buffer, re.MULTILINE | re.DOTALL)
+                #
+                # for number in spaced_numbers_19:
+                #     logging.info(number.group(0))
+                #
+                # for number in spaced_numbers_18:
+                #     logging.info(number.group(0))
+                #
+                # for number in spaced_numbers_17:
+                #     logging.info(number.group(0))
+
+                # for number in spaced_numbers_16:
+                #     logging.info(number.group(0))
+
+                # for number in spaced_numbers_15:
+                #     logging.info(number.group(0))
+                #
+                # for number in spaced_numbers_14:
+                #     logging.info(number.group(0))
+                #
+                # for number in spaced_numbers_13:
+                #     logging.info(number.group(0))
+                #
+                # for number in spaced_numbers_12:
+                #     logging.info(number.group(0))
+
+                        # numbers = re.finditer('\d{4}', file_buffer, re.MULTILINE | re.DOTALL)
+                #
+                # last_end = -2
+                #
+                # for number in numbers:
+                #     if number.start() - last_end > 0:
+                #         logging.info(last_end)
+                #         logging.info(number.start())
+                #         logging.info('Number start %s found at %d' % (number.group(0), number.start()))
+                #         last_end = number.end()
+                #     else:
+                #         last_end = number.end()
+
+                # if len(numbers) > 0:
+                #     logging.info('Found card numbers!')
+                #     for number in numbers:
+                #         logging.info('Number: %s' % number)
+
+                # numbers = re.findall('\d', file_buffer, re.MULTILINE | re.DOTALL)
+                # if len(numbers) > 0:
+                #     number_buffer = ''.join(numbers)
+                #
+                # logging.info('Here are all the numbers in the file %s' % number_buffer)
+                #
+                # for index in range(0, len(number_buffer)):
+                #     for card_length in range(12, 19):
+                #         card = Card(number_buffer[index:index + card_length])
+                #         if card.valid_luhn and card.issuer != 'Unknown':
+                #             logging.info('Found card (%s): %s' % (card.issuer, number_buffer[index:index + card_length]))
+
+    def reduce(self, numbers1, numbers2):
+        reduced_numbers = []
+        for number1 in numbers1:
+            found = False
+            for number2 in numbers2:
+                if number1.start() == number2.start():
+                    found = True
+            if not found:
+                reduced_numbers.append(number1)
+
+        return reduced_numbers
